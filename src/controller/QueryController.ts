b89da37bfa3c502cafde6019f535a48b23a56376
Spencer Austman <spencerlgaustman@gmail.com>
2016-10-05 20:14:22 -0700

    First pass at Queries and passing the information to the DatasetController

--- src/controller/QueryController.ts
+++ src/controller/QueryController.ts
@@ -4,6 +4,7 @@
 
 import {Datasets} from "./DatasetController";
 import Log from "../Util";
+import DatasetController from '../controller/DatasetController';
 
 export interface QueryRequest {
     GET: string|string[];
@@ -19,6 +20,7 @@ export interface QueryResponse {
 }
 
 export default class QueryController {
+    private static datasetController = new DatasetController();
     private datasets: Datasets = null;
 
     constructor(datasets: Datasets) {
@@ -38,7 +40,43 @@ export default class QueryController {
         // TODO: implement this (where we handle get, where, etc.)
 
         var resp: QueryResponse;
-
+        let controller = QueryController.datasetController;
+        console.log("hello" + JSON.stringify(query));
+        // For the get query
+        if (query.GET){
+            console.log("inside : " + query.GET);
+            for (var i =0; i < query.GET.length; i++){
+                switch (query.GET[i]) {
+                    case 'courses_dept':
+                        controller.queryDataset(query.GET[i]);
+                        break;
+                    case 'courses_id':
+                        controller.queryDataset(query.GET[i]);
+                        break;
+                    case 'courses_avg':
+                        controller.queryDataset(query.GET[i]);
+                        break;
+                    case 'courses_instructor':
+                        controller.queryDataset(query.GET[i]);
+                        break;
+                    case 'courses_title':
+                        controller.queryDataset(query.GET[i]);
+                        break;
+                    case 'courses_pass':
+                        controller.queryDataset(query.GET[i]);
+                        break;
+                    case 'courses_fail':
+                        controller.queryDataset(query.GET[i]);
+                        break;
+                    case 'courses_audit':
+                        controller.queryDataset(query.GET[i]);
+                        break;
+                    default:
+                        console.log("Uh oh, you sent an invalid key");
+                        break;
+                }
+            }
+        }
         //if (query.add) {
         //    let res = 0;
         //    for(var n of query.add) {
