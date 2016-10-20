2d233998c4b8f1cff68b7270d4aecc08c920a00a
Spencer Austman <spencerlgaustman@gmail.com>
2016-10-20 16:13:52 -0700

    Initial pass of InsightFacade and connecting InsightFacade and RouteHandler

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,46 +1,141 @@
 /**
- * Created by Spencer on 10/13/2016.
+ * Created by Spencer on 10/20/2016.
  */
-/*
- * This should be in the same folder as your controllers
- */
-import {QueryRequest} from "./QueryController";
+import {IInsightFacade, InsightResponse} from "./IInsightFacade";
+import Log from "../Util";
+import DatasetController from '../controller/DatasetController';
+import QueryController from "../controller/QueryController";
+var fs = require("fs");
+import {Datasets} from '../controller/DatasetController';
 
-export interface InsightResponse {
-    code: number;
-    body: {}; // this is what you would return to a requestee in the REST body
-}
+import {QueryRequest} from "../controller/QueryController";
 
-export interface IInsightFacade {
+export default class InsightFacade implements IInsightFacade {
 
-    /**
-     * Add a dataset to UBCInsight.
-     *
-     * @param id  The id of the dataset being added. This is the same as the PUT id.
-     * @param content  The base64 content of the dataset. This is the same as the PUT body.
-     *
-     * The promise should return an InsightResponse for both fulfill and reject.
-     * fulfill should be for 2XX codes and reject for everything else.
-     */
-    addDataset(id: string, content: string): Promise<InsightResponse>;
+    // TODO: need to implement this
+    //private static iinsightFacade = new IInsightFacade();
 
-    /**
-     * Remove a dataset from UBCInsight.
-     *
-     * @param id  The id of the dataset to remove. This is the same as the DELETE id.
-     *
-     * The promise should return an InsightResponse for both fulfill and reject.
-     * fulfill should be for 2XX codes and reject for everything else.
-     */
-    removeDataset(id: string): Promise<InsightResponse>;
+    private static datasetController = new DatasetController();
 
-    /**
-     * Perform a query on UBCInsight.
-     *
-     * @param query  The query to be performed. This is the same as the body of the POST message.
-     * @return Promise <InsightResponse>
-     * The promise should return an InsightResponse for both fulfill and reject.
-     * fulfill should be for 2XX codes and reject for everything else.
-     */
-    performQuery(query: QueryRequest): Promise<InsightResponse>;
+    addDataset(id:string, content:any): Promise<InsightResponse>{
+        let controller = InsightFacade.datasetController;
+        return new Promise(function (fulfill, reject) {
+            try {
+                if (controller.getDataset(id)) {
+                    controller.process(id, content).then(function (result) {
+                        Log.trace('InsightFacade::postDataset(..) - processed');
+                        Log.trace("sending 201");
+                        Log.trace("this is the result " + result);
+                        if (result) {
+                            fulfill({code: 201, body: result});
+                        }
+                        else {
+                            reject({code: 400, body: "Incorrect dataset"});
+                        }
+                    }).catch(function (err:Error) {
+                        Log.trace('InsightFacade::postDataset(..) - ERROR: ' + err.message);
+                        reject({code: 400, body: err.message});
+                    });
+                } else {
+                    controller.process(id, content).then(function (result) {
+                        Log.trace('InsightFacade::postDataset(..) - processed');
+                        Log.trace("sending 204");
+                        Log.trace("this is the result " + result);
+                        if (result) {
+                            fulfill({code: 204, body: result});
+                        }
+                        else {
+                            reject({code: 400, body: "Incorrect dataset"});
+                        }
+
+                    }).catch(function (err:Error) {
+                        Log.trace('InsightFacade::postDataset(..) - ERROR: ' + err.message);
+                        reject({code: 400, body: err.message});
+                    });
+                }
+            } catch (err) {
+                Log.trace('DatasetController::process(..) - ERROR: ' + err);
+                reject(err);
+            }
+        });
+    }
+
+    removeDataset(id:string): Promise<InsightResponse> {
+        let controller = InsightFacade.datasetController;
+        return new Promise(function (fulfill, reject) {
+            try {
+                if (controller.getDataset(id) || fs.existsSync("data/" + id + '.json')) {
+                    controller.deleteDataset(id);
+                    console.log('delete done in RouteHandler');
+                    // check if file in path has been deleted
+                    // if file in path deleted, res 204
+                    // else file in path has not been deleted for whatever reason, return an res 400 (?)
+                    if (fs.existsSync("data/" + id + '.json'))
+                        reject({code: 400, body: 'delete did not delete for some reason!'});
+                    else
+                        fulfill({code: 204, body: 'the operation was successful.'});
+                } else {
+                    // was not previously put, or has been deleted already, so 404
+                    reject({code:404, body: 'the operation was unsuccessful because the ' +
+                    ' delete was for a resource that was not previously PUT.'});
+                    console.log('tripped the DELETE 404');
+                }
+            } catch (err) {
+                Log.trace('DatasetController::process(..) - ERROR: ' + err);
+                reject(err);
+            }
+        });
+    }
+
+    performQuery(query: QueryRequest): Promise<InsightResponse> {
+        let controller = InsightFacade.datasetController;
+        return new Promise(function (fulfill, reject) {
+            try {
+                let dsController = InsightFacade.datasetController;
+                let datasets: Datasets = dsController.getDatasets();
+                let controller = new QueryController(datasets);
+                let isValid = controller.isValid(query);
+                let missing_resource = false;
+                if (isValid === true) {
+                    var invalid_ids: any[] = [];
+                    for (var i = 0; i < query.GET.length; i++){
+                        var us_index = query.GET[i].indexOf('_');
+                        var query_id = query.GET[i].substring(0, us_index);
+
+                        if (dsController.getDataset(query_id) || fs.existsSync("data/" + query_id + '.json')) {
+                            continue;
+                        } else {
+                            console.log('RouteHandler.postQuery: substring to get id from GET keys: ' + query_id);
+                            if (invalid_ids.indexOf(query_id) < 0)
+                                invalid_ids[i] = query_id;
+                            console.log("logged invalid ids: " + invalid_ids);
+                            missing_resource = true;
+                        }
+                    }
+
+                    if (missing_resource) {
+                        reject({code: 424, body: 'missing ' + invalid_ids});
+                    } else {
+                        let result = controller.query(query);
+                        console.log('RouteHandler.postQuery: result of controller.query(query)' + result);
+                        //Brendon: unsure if "if else" required. will need QueryController.query to be completed first
+                        if (result !== null){
+                            fulfill({code: 200, body: result});
+                            console.log("RouteHandler.postQuery: post query is a success!");
+                        }
+                        else {
+                            reject({code: 400, body: 'result = controller.query(query) did not return valid result?'});
+                        }
+                    }
+                } else {
+                    reject({code: 400, body: 'invalid query. query should contain GET, WHERE, ORDER, AS'});
+                }
+            } catch (err) {
+                Log.trace('DatasetController::process(..) - ERROR: ' + err);
+                reject(err);
+            }
+        });
+    }
+
+
 }
